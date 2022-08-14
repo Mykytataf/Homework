413a54aa42cd8203cf7a50802d1ce62e6bae0946
Mykyta <tafincevmykyta@gmail.com>
2022-08-14 18:38:47 +0300

    Files

---
+++ File_of_Homework5.py
@@ -0,0 +1,11 @@
+print('Введіть число')
+num = int(input())
+num1 = 0
+Char_item = ''
+print(Char_item)
+for i in Char_item + "*":
+ Char_item = Char_item + '*'
+ num1 = num1 + 1
+ print(Char_item)
+ if(num1 == num):
+  break
