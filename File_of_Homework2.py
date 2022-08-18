ce889213dd0f989b8ad25a64d9022526030835b5
Mykyta <tafincevmykyta@gmail.com>
2022-08-18 17:28:32 +0300

    Files

--- File_of_Homework2.py
+++ File_of_Homework2.py
@@ -1,3 +1,6 @@
-print('Скільки є кур?')
-chikens = input()
-legs_of_chickens = chikens * 2
+print('Скільки чашок кави ви хочете замовити?')
+caps_of_coffe = input()
+caps_of_coffe = int(caps_of_coffe)
+bonus_caps_of_coffe = caps_of_coffe // 6
+all_caps_of_coffe = caps_of_coffe + bonus_caps_of_coffe
+print(all_caps_of_coffe)
--- File_of_Homework3.py
+++ File_of_Homework3.py
@@ -1,4 +1,4 @@
-from typing import _ProtocolMeta
+
 
 
 print('Введіть хвилини')
